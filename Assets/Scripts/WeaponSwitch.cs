8c4e607ea053edecc913cbba5af33f65156496f1
ParhamGh <parhamghafourian8@gmail.com>
2025-01-30 15:41:03 +0330

    Modified weapon switch and animations

--- Assets/Scripts/WeaponSwitch.cs
+++ Assets/Scripts/WeaponSwitch.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class WeaponSwitch : MonoBehaviour
 {
     public Animator animator;
-    private int _weaponNumber = 0;
+    private int _weaponNumber = 1;
+    private int _maxWeapons = 4;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeWeapon(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) ChangeWeapon(2);
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeWeapon(3);
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) ChangeWeapon(4);
+
+        // Mouse scroll wheel selection
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0f)
         {
-            ChangeWeapon(1);
+            ChangeWeapon(_weaponNumber + 1 > _maxWeapons ? 1 : _weaponNumber + 1);
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        else if (scroll < 0f)
         {
-            ChangeWeapon(2);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ChangeWeapon(3);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            ChangeWeapon(4);
+            ChangeWeapon(_weaponNumber - 1 < 1 ? _maxWeapons : _weaponNumber - 1);
         }
     }
 
